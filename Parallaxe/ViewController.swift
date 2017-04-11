aabae06983cd5af37bf79d66b0ca7003bec9b1f2
Alexandre Torres <alexandre@imacdealexandre.home>
2017-04-11 13:50:58 +0200

    Initial Commit

---
+++ Parallaxe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parallaxe
+//
+//  Created by Alexandre Torres on 11/04/2017.
+//  Copyright © 2017 Alexandre Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

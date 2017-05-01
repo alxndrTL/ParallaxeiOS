ef79d96a75596c78612d5badf9b3dbb5501e2240
Alexandre Torres <alexandre@imacdealexandre.home>
2017-05-01 21:37:06 +0200

    new commit 3

--- Parallaxe/ViewController.swift
+++ Parallaxe/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         self.view = MaVue(frame: UIScreen.main.bounds)
         self.view.backgroundColor = UIColor.white
         
-        print("new commit 2 ! third branch ")
+        print("new commit 3 ! third branch ")
     }
 
     override func didReceiveMemoryWarning() {
